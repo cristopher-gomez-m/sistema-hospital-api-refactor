20c032b101b5eb660840f84dd6533d1438643828
cristopher-gomez-m <cristopher.gomezm@ug.edu.ec>
2023-06-21 19:47:08 -0500

    agregado main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-async function bootstrap() {
+async function bootstrap () {
   const app = await NestFactory.create<NestExpressApplication>(AppModule,{ cors: true });
   app.useGlobalPipes(new ValidationPipe());
   /*
@@ -17,29 +17,26 @@ async function bootstrap() {
   };
   */
   const config = new DocumentBuilder()
-  .setTitle('sistema-hospital-api')
-  .setDescription('Api del SistemaHospital')
-  .setVersion('1.0')
-  .addTag('endpoint') // Puedes agregar etiquetas para categorizar tus endpoints
-  .build();
+    .setTitle('sistema-hospital-api')
+    .setDescription('Api del SistemaHospital')
+    .setVersion('1.0')
+    .addTag('endpoint') // Puedes agregar etiquetas para categorizar tus endpoints
+    .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  app.use((req, res, next) => {
+  app.use((_req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
   });
   app.enableCors({
-    allowedHeaders:"*",
-    origin: "*"
-});
-  //app.enableCors(corsOptions);
-  
+    allowedHeaders: '*',
+    origin: '*',
+  });
+  // app.enableCors(corsOptions);
 
-
-  
   await app.listen(3000);
 }
 bootstrap();
